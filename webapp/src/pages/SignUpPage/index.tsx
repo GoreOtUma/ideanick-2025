fbe7351f4c41289b3555241eb4920a12f300381b
GoreOtUma <marina.5kyshnareva@mail.ru>
2025-07-20 15:47:01 +0300

    Add useForm hook

--- webapp/src/pages/SignUpPage/index.tsx
+++ webapp/src/pages/SignUpPage/index.tsx
@@ -1,8 +1,5 @@
 import { zSignUpTrpcInput } from '@ideanick/backend/src/router/signUp/input'
-import { useFormik } from 'formik'
-import { withZodSchema } from 'formik-validator-zod'
 import Cookies from 'js-cookie'
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { z } from 'zod'
 import { Alert } from '../../components/Alert'
@@ -10,54 +7,41 @@ import { Button } from '../../components/Button'
 import { FormItems } from '../../components/FormItems'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
+import { useForm } from '../../lib/form'
 import { getAllIdeasRoute } from '../../lib/routes'
 import { trpc } from '../../lib/trpc'
 
 export const SignUpPage = () => {
   const navigate = useNavigate()
   const trpcUtils = trpc.useContext()
-  const [successMessageVisible, setSuccessMessageVisible] = useState(false)
-  const [submittingError, setSubmittingError] = useState<string | null>(null)
   const signUp = trpc.signUp.useMutation()
-  const formik = useFormik({
+  const { formik, buttonProps, alertProps } = useForm({
     initialValues: {
       nick: '',
       password: '',
       passwordAgain: '',
     },
-    // @ts-ignore
-    validate: withZodSchema(
-      zSignUpTrpcInput
-        .extend({
-          passwordAgain: z.string().min(1),
-        })
-        .superRefine((val, ctx) => {
-          if (val.password !== val.passwordAgain) {
-            ctx.addIssue({
-              code: z.ZodIssueCode.custom,
-              message: 'Passwords must be the same',
-              path: ['passwordAgain'],
-            })
-          }
-        })
-    ),
+    validationSchema: zSignUpTrpcInput
+      .extend({
+        passwordAgain: z.string().min(1),
+      })
+      .superRefine((val, ctx) => {
+        if (val.password !== val.passwordAgain) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: 'Passwords must be the same',
+            path: ['passwordAgain'],
+          })
+        }
+      }),
+
     onSubmit: async (values) => {
-      try {
-        setSubmittingError(null)
-        const { token } = await signUp.mutateAsync(values)
-        Cookies.set('token', token, { expires: 99999 })
-        void trpcUtils.invalidate()
-        navigate(getAllIdeasRoute())
-        await signUp.mutateAsync(values)
-        formik.resetForm()
-        setSuccessMessageVisible(true)
-        setTimeout(() => {
-          setSuccessMessageVisible(false)
-        }, 3000)
-      } catch (err: any) {
-        setSubmittingError(err.message)
-      }
+      const { token } = await signUp.mutateAsync(values)
+      Cookies.set('token', token, { expires: 99999 })
+      void trpcUtils.invalidate()
+      navigate(getAllIdeasRoute())
     },
+    resetOnSuccess: false,
   })
 
   return (
@@ -67,10 +51,8 @@ export const SignUpPage = () => {
           <Input label='Nick' name='nick' formik={formik} />
           <Input label='Password' name='password' type='password' formik={formik} />
           <Input label='Password again' name='passwordAgain' type='password' formik={formik} />
-          {!formik.isValid && !!formik.submitCount && <Alert color='red'>Some fields are invalid</Alert>}
-          {submittingError && <Alert color='red'>{submittingError}</Alert>}
-          {successMessageVisible && <Alert color='green'>Thanks for sign up!</Alert>}
-          <Button loading={formik.isSubmitting}>Sign Up</Button>
+          <Alert {...alertProps} />
+          <Button {...buttonProps}>Sign Up</Button>
         </FormItems>
       </form>
     </Segment>
