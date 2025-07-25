2cb74171f83ec0eae659ebf79bfe863e12fb79c3
GoreOtUma <marina.5kyshnareva@mail.ru>
2025-07-25 20:55:36 +0300

    Add like icon

--- webapp/src/pages/ideas/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ideas/ViewIdeaPage/index.tsx
@@ -6,6 +6,7 @@ import { Alert } from '../../../components/Alert'
 import { Button, LinkButton } from '../../../components/Button'
 import { FormItems } from '../../../components/FormItems'
 import { Segment } from '../../../components/Segment'
+import { Icon } from '../../../components/icon'
 import { useForm } from '../../../lib/form'
 import { withPageWrapper } from '../../../lib/pageWrapper'
 import { getEditIdeaRoute, type ViewIdeaRouteParams } from '../../../lib/routes'
@@ -40,7 +41,7 @@ const LikeButton = ({ idea }: { idea: NonNullable<TrpcRouterOutput['getIdea']['i
         void setIdeaLike.mutateAsync({ ideaId: idea.id, isLikedByMe: !idea.isLikedByMe })
       }}
     >
-      {idea.isLikedByMe ? 'Unlike' : 'Like'}
+      <Icon size={32} className={css.likeIcon} name={idea.isLikedByMe ? 'likeFilled' : 'likeEmpty'} />
     </button>
   )
 }
