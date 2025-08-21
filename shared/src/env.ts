236f8cf5465de8fba077aa8d9dc9a774f2877bfa
GoreOtUma <marina.5kyshnareva@mail.ru>
2025-08-21 19:53:57 +0300

    Send public env from backend to webapp

--- shared/src/env.ts
+++ shared/src/env.ts
@@ -2,11 +2,18 @@
 import { z } from 'zod'
 import { zEnvNonemptyTrimmed, zEnvNonemptyTrimmedRequiredOnNotLocal } from './zod'
 
+declare global {
+  const webappEnvFromBackend: Record<string, string> | undefined
+}
+const windowEnv = typeof webappEnvFromBackend !== 'undefined' ? webappEnvFromBackend : {}
+const getSharedEnvVariable = (key: string) =>
+  windowEnv[`VITE_${key}`] || windowEnv[key] || process.env[`VITE_${key}`] || process.env[key]
+
 const sharedEnvRaw = {
   CLOUDINARY_CLOUD_NAME: process.env.VITE_CLOUDINARY_CLOUD_NAME || process.env.CLOUDINARY_CLOUD_NAME,
   S3_URL: process.env.VITE_S3_URL || process.env.S3_URL,
   WEBAPP_URL: process.env.VITE_WEBAPP_URL || process.env.WEBAPP_URL,
-  S3_BUCKET_NAME: process.env.VITE_S3_BUCKET_NAME || process.env.S3_BUCKET_NAME,
+  S3_BUCKET_NAME: getSharedEnvVariable('S3_BUCKET_NAME'),
 }
 
 const zEnv = z.object({
